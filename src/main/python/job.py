01680641c3a6a501e2b61552418a1a8df7154479
cxzhouyq <42760422+cxzhouyq@users.noreply.github.com>
2026-01-14 14:52:02 +0800

    Update job.py

--- src/main/python/job.py
+++ src/main/python/job.py
@@ -12,13 +12,13 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 def post_xj():
     fails = []
     weekday = datetime.today().weekday()  # Monday=0 ... Sunday=6
-    
+
     # 从配置获取数据
     config = load_config()
     xj_users = config['XJ']['users'].split(',')
     xj_domains = config['XJ']['domains'].split(',')
     ym_domains = config['YM']['domains'].split(',')
-    
+
     # 定义URL模板
     urls = [
         config['URL_TEMPLATES']['box'],
@@ -31,14 +31,14 @@ def post_xj():
     for url_template in urls:
         for user_entry in xj_users:
             user_type, cookie = user_entry.split('_', 1)
-            
+
             # 检查周六条件
             if "taskid=1622" in url_template and weekday != 5:  # 5=Saturday
                 continue
-                
+
             # 选择域名池
             domains = xj_domains if user_type == 'xj' else ym_domains
-            
+
             for domain in domains:
                 url = ''
                 try:
@@ -47,20 +47,20 @@ def post_xj():
                         url = url_template.format(domain, random.randint(1, 1000))
                     else:
                         url = url_template.format(domain)
-                        
+
                     headers = {
                         'X-Cookie-Auth': cookie,
                         'user-agent': 'Dio/4.1.8 Android'
                     }
-                    
+
                     log_info(f"请求url: {url}")
                     time.sleep(1)  # 间隔1秒
                     log_info(f"用户信息[{user_entry}]")
-                    response = requests.post(url, headers=headers)
+                    response = requests.post(url, headers=headers, timeout=30, verify=False)
                     log_info(f"Response Body: {response.text}")
 
                     # {"retcode":-9999,"errmsg":"\u60a8\u8fd8\u6ca1\u6709\u767b\u5f55","data":{"xxx_api_auth":"6362376533663933643735663137376664343762386434663531376666356538"}}
-                    
+
                     if response.status_code == 200:
                         try:
                             response_data = response.json()
@@ -71,12 +71,12 @@ def post_xj():
                             fails.append(f"{url}({user_entry})请求失败，非json返回值")
                     elif domain == domains[-1]:
                         fails.append(f"{url}({user_entry})请求失败！")
-                        
+
                 except Exception as e:
                     log_error(e)
                     if domain == domains[-1]:
                         fails.append(f"{url}({user_entry})请求失败！")
-    
+
     return fails
 
 
@@ -94,7 +94,7 @@ def post_vip():
                 'user-agent': 'Dio/4.1.8 Android'
             }
             try:
-                response = requests.post(domain, headers=headers)
+                response = requests.post(domain, headers=headers, timeout=30, verify=False)
                 log_info(f"请求url: {domain}")
                 log_info(f"Response Body: {response.text}")
                 try:
@@ -128,10 +128,10 @@ def vip_sign():
 
 if __name__ == "__main__":
 
-     
+
     # 立即执行一次
-    # xj_sign()
-    # vip_sign()
+    xj_sign()
+    vip_sign()
 
 # 每天 10:01 执行
     scheduler = BlockingScheduler()
